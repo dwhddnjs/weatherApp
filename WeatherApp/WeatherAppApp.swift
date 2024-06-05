a6e7e17c1e799a6f32d0498a5f7ba0d3297f32f5
dwhddnjs <per1215@naver.com>
2024-06-06 05:45:37 +0900

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by 이종원 on 6/6/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
